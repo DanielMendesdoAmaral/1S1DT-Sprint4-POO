4adc57293347c293d9618f111a8ed3263bafbe87
Daniel Amaral <daniel.amaral720@gmail.com>
2020-06-05 14:29:25 -0300

    Aula 11

---
+++ Aula 11- Polimorfismo por sobrecarga/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula_11__Polimorfismo_por_sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo c = new Calculo();
+
+            Console.WriteLine($"{  c.Calcular()  }\n{  c.Calcular(5, 5, '+')  }\n{  c.Calcular("")  }\n{  c.Calcular("(7/5)*2")  }");
+        }
+    }
+}
