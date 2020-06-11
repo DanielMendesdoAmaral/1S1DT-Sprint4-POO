0ccc1b37c43c1df06fa557342f11004535fb4f6e
Daniel Amaral <daniel.amaral720@gmail.com>
2020-06-11 14:39:43 -0300

    Aula 15

---
+++ Aula 15- Try-Catch/Permissao.cs
@@ -0,0 +1,27 @@
+namespace Aula_15__Try_Catch
+{
+    public class Permissao
+    {
+        private bool permitir;
+        public bool Permitir
+        {
+            get { return permitir; }
+            set { permitir = value; }
+        }
+        
+        public void Autorizar() {
+            System.Console.Write("Deseja permitir acesso? [true/false] ");
+            try {
+                Permitir = System.Convert.ToBoolean( System.Console.ReadLine() );
+                System.Console.ForegroundColor = System.ConsoleColor.Green;
+                System.Console.WriteLine(Permitir);
+                System.Console.ForegroundColor = System.ConsoleColor.White;
+            }
+            catch {
+                System.Console.ForegroundColor = System.ConsoleColor.Red;
+                System.Console.WriteLine("ERRO! DADO INVÁLIDO. DIGITE true OU false.");
+                System.Console.ForegroundColor = System.ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ Aula 15- Try-Catch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula_15__Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Permissao permissao = new Permissao();
+            permissao.Autorizar(); //Se você colocar uma entrada diferente de true ou false, ele dará uma exceção.
+        }
+    }
+}
