eb4f92fb144d82167683067c1b5d8b9890d09a83
Daniel Amaral <daniel.amaral720@gmail.com>
2020-06-30 15:17:04 -0300

    Aula 28

--- Aulas_27-30-CRUD/Produto.cs
+++ Aulas_27-30-CRUD/Produto.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System;
 
 namespace Aulas_27_30_CRUD
 {
@@ -23,7 +25,7 @@ namespace Aulas_27_30_CRUD
             }
             //A partir daqui, o diretório já existe. Então, verifica-se a existência do arquivo. Se este não existir, será criado aqui.
             if(!File.Exists(PATHARCHIVE)) {
-                File.Create(PATHARCHIVE).Close();
+                File.Create(PATHARCHIVE).Close(); //Cria-se e logo em seguida o fecha. Sempre faça isso se possível.
             }
         }
 
@@ -43,6 +45,43 @@ namespace Aulas_27_30_CRUD
             var linha = new string[] {PrepararLinha()}; 
             //Este método necessita do caminho onde se quer gravar os dados e de um array de dados.
             File.AppendAllLines(PATHARCHIVE, linha);
+        }
+
+        /// <summary>
+        ///     Pega todas as linhas do banco de dados e guarda em um array. Em seguida, percorre-se cada linha deste array e separa todos os dados em outro array. Em seguida instancia-se um objeto com todos esses dados (um objeto para cada registro do banco de dados) e o adiciona em uma lista de produtos. Em seguida, retorna-se esta lista.
+        /// </summary>
+        /// <returns>Uma lista com todos os produtos.</returns>
+        public static List<Produto> Ler() {
+            List<Produto> produtos = new List<Produto>();
+            //Array que em cada posição guarda uma linha do banco de dados.
+            string[] linhas = File.ReadAllLines(PATHARCHIVE);
+
+            //Agora percorremos todos os registros, todas as linhas do banco de dados.
+            foreach(string linha in linhas) {
+                //Separamos agora cada dado de cada linha.
+                string[] dado = linha.Split(";");
+                //Criamos um novo produto passando os dados já separados.
+                Produto produto = new Produto( Int32.Parse(dado[0]), dado[1], Double.Parse(dado[2]) );
+                //Adicionamos o produto na lista de produtos, que será retornada assim que todos os produtos estiverem nela.
+                produtos.Add(produto);
+            }
+
+            return produtos;
+        }
+
+        /// <summary>
+        ///     Uma sobrecarga do método Ler() que reaproveita o próprio método Ler() para achar uma instância com o nome especificado por um parâmetro.
+        /// </summary>
+        /// <param name="nome">Nome para filtro.</param>
+        /// <returns>Retorna uma lista com todos os objetos com o nome especificado.</returns>
+        public static List<Produto> Ler(string nome) {
+            List<Produto> produtos = new List<Produto>();
+            foreach( Produto produto in Ler() ) {
+                if(produto.Nome==nome) {
+                    produtos.Add(produto);
+                }
+            }
+            return produtos;
         }
     }
 }
--- Aulas_27-30-CRUD/Program.cs
+++ Aulas_27-30-CRUD/Program.cs
@@ -1,5 +1,6 @@
 ﻿//CRUD feito em 4 aulas. 
 //Desafio proposto na aula 27 solucionado: Apagar a pasta e o arquivo .csv gera um erro. Para isso, eu decidi criar a pasta separada do arquivo. Assim, toda vez que um deles forem apagados, ao rodar o programa novamente não irá gerar o erro.
+//Desafio proposto na aula 28 solucionado: Faça um método que aproveite o método Ler() para realizar uma busca por nome, passado como argumento. Dica: utilize expressão Lambda. Com a expressão lambda só estava conseguindo printar 1.. mas com o if consegui achar todos os que tem o nome especificado.
 
 using System;
 
@@ -9,17 +10,19 @@ namespace Aulas_27_30_CRUD
     {
         static void Main(string[] args)
         {
-            Produto p1 = new Produto(1, "GTA V", 34.99);
-            Produto p2 = new Produto(2, "Red Dead Redemption", 139.99);
-            Produto p3 = new Produto(3, "Battlefield V", 85.40);
-            Produto p4 = new Produto(4, "Fortnite", 0);
-            Produto p5 = new Produto(5, "COD Modern Warfare", 197.90);
+            Console.Clear();
+            
+            //Percorremos cada produto de nossa lista de produtos do método Ler().
+            foreach( Produto produto in Produto.Ler() ) {
+                Console.WriteLine($"{produto.Nome} - {produto.Preco}");
+            }
 
-            p1.Cadastrar();
-            p2.Cadastrar();
-            p3.Cadastrar();
-            p4.Cadastrar();
-            p5.Cadastrar();
+            Console.WriteLine();
+
+            //Mostramos no console os produtos com nome de GTA V
+            foreach( Produto produto in Produto.Ler("GTA V") ) {
+                Console.WriteLine($"{produto.Nome} - {produto.Preco}");
+            }
         }
     }
 }
