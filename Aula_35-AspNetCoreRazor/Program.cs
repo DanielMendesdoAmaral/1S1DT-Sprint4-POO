b0f45fd2532218faadf7e3d8ba3f5b46cca68dd0
Daniel Amaral <daniel.amaral720@gmail.com>
2020-07-09 15:00:36 -0300

    Aula 35

---
+++ Aula_35-AspNetCoreRazor/Program.cs
@@ -0,0 +1,29 @@
+//ASP NET = FULLSTACK
+//RAZOR = SINTAXE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aula_35_AspNetCoreRazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
